a8753b23ed85c05be96c07cff7d66d0a018afc3a
LucasEspaladori <leskenazi25@ubishops.ca>
2025-10-27 11:20:30 -0400

    lab6 final changes

--- nav.js
+++ nav.js
@@ -5,23 +5,37 @@ function splitAtRoot(path) {
 }
 
 function setNav(current_path) {
-    current_path = splitAtRoot(current_path);
 
     fetch("nav.html")
         .then(r => r.text())
         .then(html => {
             document.getElementById("main-nav").innerHTML = html;
-            const nav = document.getElementById("main-nav");
-            const links = nav.querySelectorAll('a');
+            current_path = splitAtRoot(current_path);
+            console.log("current page:" + current_path)
 
-            for (let child of links) {
+            let nav_ul = document.getElementById("nav_ul");
+
+            for (let li of nav_ul.children) {
+                child = li.children[0];
                 if (child instanceof HTMLAnchorElement) {
+                    console.log("Child link: " + child.href)
+                    if(splitAtRoot(child.href) === current_path){
+                        child.classList.add("current_page")
+                    }
+
                     const link_path = splitAtRoot(child.href);
+
                     if (link_path === current_path) {
+                        console.log("Got it")
                         child.classList.add("current_page"); 
+                    }
+
+                    if((current_path === "/LucasEspaladori.github.io/index.html" && "/LucasEspaladori.github.io/") &&
+                        child.href === "/LucasEspaladori.github.io/index.html"){
+                        console.log("Home too because split at root is" + splitAtRoot(child.href))
+                        child.classList.add("current_page")
                     }
                 }
             }
         })
-        .catch(e => {});
 }
